ac0b3be5cd7935452342b3360362701c3c8be3a1
Gao Shengxu <1059067158@qq.com>
2016-03-04 18:06:52 +0800

    processing.
    do some improvements.

--- AirplanePanel/MainWindow.xaml.cs
+++ AirplanePanel/MainWindow.xaml.cs
@@ -25,6 +25,9 @@ namespace AirplanePanel
             InitializeComponent();
         }
 
+         SolidColorBrush gray = new SolidColorBrush(Color.FromArgb(255, 193, 195, 189));
+         SolidColorBrush green = new SolidColorBrush(Color.FromArgb(255, 50, 255, 18));
+         SolidColorBrush black = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
         
 
         private void btn_up_Click(object sender, RoutedEventArgs e)
@@ -32,11 +35,12 @@ namespace AirplanePanel
             if (FlyCondition == 1)
             {
                 this.status.Text = "上升";
-                this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255,255,0,0));
-                this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
+                this.btn_up.Background = gray;
+                this.btn_down.Background = black;
+                this.btn_accelerate.Background = black;
+                this.btn_decelerate.Background = black;
+                this.btn_crab.Background = black;
+                this.btn_levelflight.Background = gray;
                 
             }
             
@@ -48,12 +52,12 @@ namespace AirplanePanel
             if (FlyCondition == 1)
             {
                 this.status.Text = "下降";
-                this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0)); 
-                this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-
+                this.btn_up.Background = black;
+                this.btn_down.Background = gray;
+                this.btn_accelerate.Background = black;
+                this.btn_decelerate.Background = black;
+                this.btn_crab.Background = black;
+                this.btn_levelflight.Background = gray;
             }
            
         }
@@ -63,11 +67,12 @@ namespace AirplanePanel
             if (FlyCondition == 1)
             {
                 this.status.Text = " 加速";
-                this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
-                this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
+                this.btn_up.Background = black;
+                this.btn_down.Background = black;
+                this.btn_accelerate.Background = gray;
+                this.btn_decelerate.Background = black;
+                this.btn_crab.Background = black;
+                this.btn_levelflight.Background = gray;
             }
         }
 
@@ -76,11 +81,12 @@ namespace AirplanePanel
             if (FlyCondition == 1)
             {
                 this.status.Text = " 减速";
-                this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
-                this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
+                this.btn_up.Background = black;
+                this.btn_down.Background = black;
+                this.btn_accelerate.Background = black;
+                this.btn_decelerate.Background = gray;
+                this.btn_crab.Background = black;
+                this.btn_levelflight.Background = gray;
             }
         }
 
@@ -89,11 +95,12 @@ namespace AirplanePanel
             if (FlyCondition == 1)
             {
                 this.status.Text = " 侧飞";
-                this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-                this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                this.btn_up.Background = black;
+                this.btn_down.Background = black;
+                this.btn_accelerate.Background = black;
+                this.btn_decelerate.Background = black;
+                this.btn_crab.Background = gray;
+                this.btn_levelflight.Background = gray;
             }
         }
 
@@ -103,15 +110,19 @@ namespace AirplanePanel
         {
             this.status.Text = "  ";
             FlyCondition = 0;
-            this.GroupBox_launch.Background = new SolidColorBrush(Colors.LightBlue);
-            this.GroupBox_levelflight.Background = new SolidColorBrush(Colors.White);
-            this.GroupBox_landing.Background = new SolidColorBrush(Colors.White);
+            this.GroupBox_launch.Background = black;
+            this.GroupBox_levelflight.Background = new SolidColorBrush(Colors.Black);
+            this.GroupBox_landing.Background = new SolidColorBrush(Colors.Black);
 
-            this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
+            this.btn_up.Background = black;
+            this.btn_down.Background = black;
+            this.btn_accelerate.Background = black;
+            this.btn_decelerate.Background = black;
+            this.btn_crab.Background = black;
+
+            this.btn_levelflight.Background = black;
+            this.btn_launch.Background = gray;
+            this.btn_landing.Background = black;
         }
 
         
@@ -119,15 +130,19 @@ namespace AirplanePanel
         private void Button_Click_levelflight(object sender, RoutedEventArgs e)
         {
             FlyCondition = 1;
-            this.GroupBox_levelflight.Background = new SolidColorBrush(Colors.LightBlue);
-            this.GroupBox_launch.Background = new SolidColorBrush(Colors.White);
-            this.GroupBox_landing.Background = new SolidColorBrush(Colors.White);
+            this.GroupBox_levelflight.Background = black;
+            this.GroupBox_launch.Background = new SolidColorBrush(Colors.Black);
+            this.GroupBox_landing.Background = new SolidColorBrush(Colors.Black);
 
-            this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
+            this.btn_up.Background = black;
+            this.btn_down.Background = black;
+            this.btn_accelerate.Background = black;
+            this.btn_decelerate.Background = black;
+            this.btn_crab.Background = black;
+
+            this.btn_levelflight.Background = gray;
+            this.btn_launch.Background = black;
+            this.btn_landing.Background = black;
        
         }
 
@@ -135,35 +150,55 @@ namespace AirplanePanel
         {
             FlyCondition = 2;
             this.status.Text = "  ";
-            this.GroupBox_landing.Background = new SolidColorBrush(Colors.LightBlue);
-            this.GroupBox_levelflight.Background = new SolidColorBrush(Colors.White);
-            this.GroupBox_launch.Background = new SolidColorBrush(Colors.White);
+            this.GroupBox_landing.Background = black;
+            this.GroupBox_levelflight.Background = new SolidColorBrush(Colors.Black);
+            this.GroupBox_launch.Background = new SolidColorBrush(Colors.Black);
 
-            this.btn_up.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_down.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_accelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_decelerate.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
-            this.btn_crab.Background = new SolidColorBrush(Color.FromArgb(255, 205, 205, 205));
+            this.btn_up.Background = black;
+            this.btn_down.Background = black;
+            this.btn_accelerate.Background = black;
+            this.btn_decelerate.Background = black;
+            this.btn_crab.Background = black;
+
+            this.btn_levelflight.Background = black;
+            this.btn_launch.Background = black;
+            this.btn_landing.Background = gray;
         }
 
        
 
         private void Button_WindRight_Click(object sender, RoutedEventArgs e)
         {
-            this.Button_WindRight.Foreground = new SolidColorBrush(Colors.Red);
-            this.Button_WindLeft.Foreground = new SolidColorBrush(Colors.Black);
+            this.Button_WindRight.Foreground = black;
+            this.Button_WindLeft.Foreground = green;
             this.Button_WindRight.Background = new SolidColorBrush(Colors.Yellow);
-            this.Button_WindLeft.Background = new SolidColorBrush(Colors.White);
+            this.Button_WindLeft.Background = black;
 
         }
 
         private void Button_WindLeft_Click(object sender, RoutedEventArgs e)
         {
-            this.Button_WindLeft.Foreground = new SolidColorBrush(Colors.Red);
-            this.Button_WindRight.Foreground = new SolidColorBrush(Colors.Black);
+            this.Button_WindLeft.Foreground = black;
+            this.Button_WindRight.Foreground = green;
             this.Button_WindLeft.Background = new SolidColorBrush(Colors.Yellow);
-            this.Button_WindRight.Background = new SolidColorBrush(Colors.White);
+            this.Button_WindRight.Background = black;
         }
+
+        //private void Button_Daytime_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.Button_Daytime.Foreground = black;
+        //    this.Button_Night.Foreground = green;
+        //    this.Button_Daytime.Background = new SolidColorBrush(Colors.Yellow);
+        //    this.Button_Night.Background = black;
+        //}
+
+        //private void Button_Night_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.Button_Night.Foreground = black;
+        //    this.Button_Daytime.Foreground = green;
+        //    this.Button_Night.Background = new SolidColorBrush(Colors.Yellow);
+        //    this.Button_Daytime.Background = black;
+        //}
 
       
        
